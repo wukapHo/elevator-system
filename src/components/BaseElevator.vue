6256e2dd39123e95753f1e3b7335056aa978e187
Vyacheslav Likashkov <xb0ksx@gmail.com>
2022-06-25 16:29:20 +0300

    feat: add component templates

---
+++ src/components/BaseElevator.vue
@@ -0,0 +1,37 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'BaseElevator',
+
+  props: {
+    floorsCount: {
+      type: Number,
+      default: 5,
+    },
+
+    elevatorState: {
+      type: Object,
+      default: () => ({
+        targetFloor: null,
+        isBusy: false,
+      }),
+    },
+  },
+
+  data: () => ({
+    movingTime: 1000,
+    waitingTime: 3000,
+  }),
+
+  methods: {
+    move() {},
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CallButtons.vue
@@ -0,0 +1,28 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'CallButtons',
+
+  props: {
+    floorsCount: {
+      type: Number,
+      default: 5,
+    },
+  },
+
+  emits: {
+    call: null,
+  },
+
+  methods: {
+    call() {},
+  },
+};
+</script>
+
+<style>
+
+</style>
